7dba9ba29025af5370b40b0b4304e1f6b44d2837
Eric Lewis <ericl16384@gmail.com>
2022-07-05 08:11:05 -0700

    added vault and tower

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-class CameraTransform {
+class Camera {
     constructor() {
         this.position = new Vector(0, 0);
         this.scale = 1
@@ -23,6 +23,14 @@ class CameraTransform {
     //    return new Vector(x, y);
     //}
 
+    transformScale(v) {
+        return v.mul(this.scale);
+    }
+
+    reverseScale(v) {
+        return v.div(this.scale);
+    }
+
     transform(v) {
         //return v.sub(this.position).mul(this.scale).rotate(-this.rotation);
         return v.sub(this.position).mul(this.scale);
@@ -42,33 +50,52 @@ class Map {
 
         this.tiles = createTileSpace(width, height, "grass");
 
-        this.camera = new CameraTransform();
-        this.colors = {
+        this.camera = new Camera();
+        this.tileColors = {
             //"empty": WHITE,
             //"stone": BLACK
             
             "grass": "#228B22",
             "wall": "#404040"
         }
+
+        this.towers = [];
+
+        this.vaultPosition = new Vector(10, 10);
     }
 
     draw(ctx) {
+        // map
         for(let x=0; x<this.width; x++) {
             for(let y=0; y<this.height; y++) {
                 if(this.tiles[x][y] !== null) {
-                    var color = this.colors[this.tiles[x][y]];
+                    var color = this.tileColors[this.tiles[x][y]];
                     if(color === undefined) {
                         color = PURPLE;
                     }
-                    drawPolygon(ctx, [
+                    //drawPolygon(ctx, [
+                    //    this.camera.transform(new Vector(x, y)).arr(),
+                    //    this.camera.transform(new Vector(x+1, y)).arr(),
+                    //    this.camera.transform(new Vector(x+1, y+1)).arr(),
+                    //    this.camera.transform(new Vector(x, y+1)).arr()
+                    //], color, GREY);
+
+                    // this is simpler, because I have disabled rotation
+                    drawRectangle(ctx,
                         this.camera.transform(new Vector(x, y)).arr(),
-                        this.camera.transform(new Vector(x+1, y)).arr(),
-                        this.camera.transform(new Vector(x+1, y+1)).arr(),
-                        this.camera.transform(new Vector(x, y+1)).arr()
-                    ], color, GREY);
+                        [this.camera.scale, this.camera.scale],
+                    color, GREY);
                 }
             }
         }
+
+        // vault
+        drawRectangle(ctx,
+            this.camera.transform(this.vaultPosition).arr(), [this.camera.scale, this.camera.scale],
+        YELLOW);
+
+        // towers
+        this.towers.forEach(tower => tower.draw(ctx, this.camera));
     }
 }
 
@@ -76,23 +103,26 @@ class Tower {
     constructor(position) {
         this.position = position;
 
-        this.size = new Vector(1, 1);
+        this.radius = 0.4;
         this.color = BLUE;
     }
 
-    draw(ctx, tileSize) {
-        drawRectangle(ctx, this.position.mul(tileSize).arr(), this.size.mul(tileSize).arr(), BLUE);
+    draw(ctx, camera) {
+        //drawRectangle(ctx, this.position.mul(tileSize).arr(), this.size.mul(tileSize).arr(), BLUE);
+        drawCircle(ctx,
+            camera.transform(this.position.add(0.5)).arr(), this.radius*camera.scale,
+        this.color);
     }
 }
 
 
-var map = new Map(40, 20);
+var map = new Map(50, 50);
 
 // add basic castle
 var castleX = 7;
 var castleY = 7;
-var castleW = 6;
-var castleH = 6;
+var castleW = 7;
+var castleH = 7;
 for(let i=0; i<castleW; i++) {
     map.tiles[castleX+i][castleY] = "wall";
 }
@@ -106,9 +136,10 @@ for(let i=0; i<castleH; i++) {
     map.tiles[castleX+castleW-1][castleY+i] = "wall";
 }
 
-
 map.camera.scale = 25;
 map.camera.position = new Vector(0, 0);
+
+map.towers.push(new Tower(new Vector(8, 7)));
 
 
 function draw() {
